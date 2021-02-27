d712f2877b6cd8a47c4ea46611c35c8055d4276d
franco646 <franco@>
2021-02-27 13:25:38 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ import { useAuth } from './contexts/auth-context';
 
 let BASE_URL;
 if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'sitema-de-alquiler-de-autos.herokuapp.com';
+  BASE_URL = '';
 } else if (process.env.NODE_ENV === 'development') {
   BASE_URL = 'http://localhost:8080';
 }
